34bdf6c84412cc02833877fb35c5e1c19967e3e9
huangtao <aemon@thuang.>
2026-01-08 11:02:56 +0800

    init porject

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,5 +1,60 @@
+
+macro(get_srcs_includes)
+    aux_source_directory(${CMAKE_CURRENT_SOURCE_DIR} SRC)
+    # 对内接口
+    FILE(GLOB H_FILE CONFIGURE_DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/*.h )
+    # 对外接口
+    FILE(GLOB H_FILE_I CONFIGURE_DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/include/*.h  
+        EXCLUDE  ${CMAKE_CURRENT_SOURCE_DIR}/include/${name}_export.h 
+    )   
+endmacro()
+
+
+macro(cc_executable_internal)
+    get_srcs_includes()
+
+    add_executable(${name} ${SRC} ${H_FILE} ${H_FILE_I})
+
+    if (MSVC)
+        set_target_properties(${name} PROPERTIES 
+            COMPILE_FLAGS "-bigobj"
+        )
+    endif()
+
+    math(EXPR size ${ARGC}-1)
+    if (size GREATER_EQUAL 1) 
+        foreach(i RANGE 1 ${size}) #1,2,...,${size}
+            #message(${i} " " ${ARGV${i}})
+            target_link_libraries(${name} PRIVATE ${ARGV${i}})
+        endforeach()
+    endif()
+
+endmacro()
+
+
+
+function(cc_test name)
+    cc_executable_internal(${ARGV})
+
+    build_output(${name} ${BUILD_OUTPUT_TEST_DIR})
+
+    add_test(NAME ${name} COMMAND ${name})
+endfunction()
+
+
+function(cc_executable name)
+
+    cc_executable_internal(${ARGV})
+
+    build_output(${name} ${BUILD_OUTPUT_DIR})
+    install(FILES ${name} DESTINATION bin)
+endfunction()
+
+
+
 function(cc_library name)
-    message(STATUS "=====Building library ${name}=======")
+    message(STATUS "=====Building library ${name}========")
+
     set(LIB_NAME ${name})
     configure_file(${CMAKE_SOURCE_DIR}/cmake/dll.h.in ${CMAKE_CURRENT_SOURCE_DIR}/include/${name}_export.h)
 
@@ -10,17 +65,9 @@ function(cc_library name)
         set(TYPE SHARED)
     endif()
 
-
-    aux_source_directory(${CMAKE_CURRENT_SOURCE_DIR} SRC)
-    # 对内接口
-    FILE(GLOB H_FILE CONFIGURE_DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/*.h )
-    # 对外接口
-    FILE(GLOB H_FILE_I CONFIGURE_DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/include/*.h  
-        EXCLUDE  ${CMAKE_CURRENT_SOURCE_DIR}/include/${name}_export.h 
-    )
+    get_srcs_includes()
 
     add_library(${name} ${TYPE} ${SRC}  ${H_FILE} ${H_FILE_I}) 
-
 
 
     get_target_property(CONFIG_INCLUDE_DIRS config INTERFACE_INCLUDE_DIRECTORIES)
@@ -33,12 +80,9 @@ function(cc_library name)
        PRIVATE ${CONFIG_INCLUDE_DIRS}
     )
 
-
-
-
-    target_compile_features(${name} PRIVATE 
-        cxx_std_20
-    )
+    # target_compile_features(${name} PRIVATE 
+    #     cxx_std_20
+    # )
 
     if (MSVC)
         set_target_properties(${name} PROPERTIES 
@@ -46,7 +90,60 @@ function(cc_library name)
         )
     endif()
 
+    if(${name}_SHARED)
+        target_compile_definitions(${name} PUBLIC ${name}_EXPORTS)
+    else()
+        target_compile_definitions(${name} PUBLIC ${name}_STATIC)
+    endif()
 
+    build_output(${name} ${BUILD_OUTPUT_DIR})
+
+
+    if (MSVC)
+        set_target_properties(${name} PROPERTIES 
+            DEBUG_POSTFIX "d"
+        )
+    endif()
+
+
+    set_target_properties(${name} PROPERTIES 
+        PUBLIC_HEADER "${H_FILE_I}"
+    )
+
+    install_func(${name})
+
+    message(STATUS "==================================")
+endfunction()
+
+
+
+
+function(install_func name)
+    install(TARGETS ${name}
+        EXPORT ${name}
+        RUNTIME DESTINATION bin
+        LIBRARY DESTINATION lib
+        ARCHIVE DESTINATION lib
+        PUBLIC_HEADER DESTINATION include
+    )
+
+    install(EXPORT ${name} FILE  ${name}Config.cmake
+        DESTINATION lib/cmake/${name}-${VERSION}/
+    )
+
+
+    set(GENERATE_CMAKE_DIR ${BUILD_OUTPUT_DIR}/cmake/${name}-${VERSION})
+    include(CMakePackageConfigHelpers)
+    write_basic_package_version_file(
+        ${GENERATE_CMAKE_DIR}/${name}ConfigVersion.cmake
+        VERSION ${VERSION}
+        COMPATIBILITY SameMajorVersion
+    )
+
+    install(FILES ${GENERATE_CMAKE_DIR}/${name}ConfigVersion.cmake
+        DESTINATION lib/cmake/${name}-${VERSION}/
+    )
+endfunction()
 
 
 #[[
@@ -81,57 +178,7 @@ function(cc_library name)
 #     )
 
 
-    if(${name}_SHARED)
-        target_compile_definitions(${name} PUBLIC ${name}_EXPORTS)
-    else()
-        target_compile_definitions(${name} PUBLIC ${name}_STATIC)
-    endif()
-
-    set(BUILD_OUTPUT_DIR ${CMAKE_SOURCE_DIR}/build_out)
-
-    build_output(${name})
-
-
-    if (MSVC)
-        set_target_properties(${name} PROPERTIES 
-            DEBUG_POSTFIX "d"
-        )
-    endif()
-
-    set_target_properties(${name} PROPERTIES 
-        PUBLIC_HEADER "${H_FILE_I}"
-    )
-
-    install(TARGETS ${name}
-        EXPORT ${name}
-        RUNTIME DESTINATION bin
-        LIBRARY DESTINATION lib
-        ARCHIVE DESTINATION lib
-        PUBLIC_HEADER DESTINATION include
-    )
-
-    install(EXPORT ${name} FILE  ${name}Config.cmake
-        DESTINATION lib/cmake/${name}-${VERSION}/
-    )
- 
-    
-    set(GENERATE_CMAKE_DIR ${BUILD_OUTPUT_DIR}/cmake/${name}-${VERSION})
-    include(CMakePackageConfigHelpers)
-    write_basic_package_version_file(
-        ${GENERATE_CMAKE_DIR}/${name}ConfigVersion.cmake
-        VERSION ${VERSION}
-        COMPATIBILITY SameMajorVersion
-    )
-
-    install(FILES ${GENERATE_CMAKE_DIR}/${name}ConfigVersion.cmake
-        DESTINATION lib/cmake/${name}-${VERSION}/
-    )
-
-    message(STATUS "==================================")
-endfunction()
-
-
-function(build_output name)
+function(build_output name output_dir)
     set(CONF_TYPES Debug Release MinSizeRel RelWithDebInfo) 
     list(APPEND CONF_TYPES "")
     foreach(type IN LISTS CONF_TYPES)
@@ -140,12 +187,11 @@ function(build_output name)
             string(TOUPPER _${type} conf)
         endif()
         set_target_properties(${name} PROPERTIES 
-            RUNTIME_OUTPUT_DIRECTORY${conf} ${BUILD_OUTPUT_DIR}/bin #dll pdb exe 执行程序
-            LIBRARY_OUTPUT_DIRECTORY${conf} ${BUILD_OUTPUT_DIR}/lib # .so .dylib 动态库导出文件
-            ARCHIVE_OUTPUT_DIRECTORY${conf} ${BUILD_OUTPUT_DIR}/lib # .lib .a 静态库导出文件
-            PDB_OUTPUT_DIRECTORY${conf} ${BUILD_OUTPUT_DIR}/bin #pdb
+            RUNTIME_OUTPUT_DIRECTORY${conf} ${output_dir}/bin #dll pdb exe 执行程序
+            LIBRARY_OUTPUT_DIRECTORY${conf} ${output_dir}/lib # .so .dylib 动态库导出文件
+            ARCHIVE_OUTPUT_DIRECTORY${conf} ${output_dir}/lib # .lib .a 静态库导出文件
+            PDB_OUTPUT_DIRECTORY${conf} ${output_dir}/bin #pdb
         )
     endforeach()
-    
 endfunction()
 
